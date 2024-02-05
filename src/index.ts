9c4d47d6c6f0d93fdb07235ffb3a93605cd6490c
Jongsik <jongsik@cloudflare.com>
2024-02-06 02:47:42 +0900

    favicon update

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { serveStatic } from 'hono/cloudflare-workers';
 
 const app: Hono = new Hono();
 
+app.get('/static/404.html', serveStatic({ root: './' }));
+app.get('/static/500.html', serveStatic({ root: './' }));
+app.get('/favicon.ico', serveStatic({ root: './' }));
 app.get('/static/*', serveStatic({ root: './' }));
-
 app.get('*', async (c) => {
 	const res: Response = await fetch(c.req.url);
 	const acceptHeader: string | undefined = c.req.header('Accept');
@@ -13,11 +15,12 @@ app.get('*', async (c) => {
 	if (isHTMLContentTypeAccepted) {
 		switch (res.status) {
 			case 404: {
-				return c.redirect('/static/404.html', 404);
+				return c.redirect('/static/404.html', 302);
 				break;
 			}
 			case 500: {
-				return c.redirect('/static/500.html', 500);
+				c.status(500);
+				return c.redirect('/static/500.html', 302);
 				break;
 			}
 			default: {
