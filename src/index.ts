81f079a62591a88721b5f2843b4203e644cc4ea2
Jongsik <jongsik@cloudflare.com>
2024-02-05 22:03:36 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,41 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Context, Hono } from 'hono';
+import { serveStatic } from 'hono/cloudflare-workers';
+// @ts-ignore
+import manifest from '__STATIC_CONTENT_MANIFEST';
 
-import handleProxy from './proxy';
-import handleRedirect from './redirect';
-import apiRouter from './router';
+const app: Hono = new Hono();
 
-// Export a default object containing event handlers
-export default {
-	// The fetch handler is invoked when this worker receives a HTTP(S) request
-	// and should return a Response (optionally wrapped in a Promise)
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
-		const url = new URL(request.url);
+app.get('/static/*', serveStatic({ root: './', manifest }));
+app.get('/favicon.ico', serveStatic({ path: './favicon.ico', manifest }));
+app.get('/404.html', serveStatic({ path: './404.html', manifest }));
+app.get('/500.html', serveStatic({ path: './500.html', manifest }));
 
-		// You can get pretty far with simple logic like if/switch-statements
-		switch (url.pathname) {
-			case '/redirect':
-				return handleRedirect.fetch(request, env, ctx);
+app.get('/route', async (c: Context) => {
+	const res: Response = await fetch(c.req.url);
+	const acceptHeader: string | undefined = c.req.header('Accept');
 
-			case '/proxy':
-				return handleProxy.fetch(request, env, ctx);
+	const isHTMLContentTypeAccepted: Boolean = typeof acceptHeader === 'string' && acceptHeader.indexOf('text/html') >= 0;
+	if (isHTMLContentTypeAccepted) {
+		switch (res.status) {
+			case 404: {
+				return c.redirect('/404.html', 301);
+			}
+			case 500: {
+				return c.redirect('/500.html', 301);
+			}
+			default: {
+				return res;
+			}
 		}
+	}
+	return c.text('works?', 200);
+});
 
-		if (url.pathname.startsWith('/api/')) {
-			// You can also use more robust routing
-			return apiRouter.handle(request);
-		}
+app.get('/error/404', async (c: Context) => {
+	return c.redirect('/404.html', 301);
+});
+app.get('/error/500', async (c: Context) => {
+	return c.redirect('/500.html', 301);
+});
 
-		return new Response(
-			`Try making requests to:
-      <ul>
-      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-			{ headers: { 'Content-Type': 'text/html' } }
-		);
-	},
-};
+export default app;
