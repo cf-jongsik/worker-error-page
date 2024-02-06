1a092e42ca1a331c7d152001ad7ccc5dfcfb3295
Jongsik <jongsik@cloudflare.com>
2024-02-06 11:23:50 +0900

    using import with status code

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,24 @@
 import { Hono } from 'hono';
 import { serveStatic } from 'hono/cloudflare-workers';
+import html404 from '../assets/static/404.html';
+import html500 from '../assets/static/500.html';
 
 const app: Hono = new Hono();
 
-app.get('/static/404.html', serveStatic({ root: './' }));
-app.get('/static/500.html', serveStatic({ root: './' }));
 app.get('/favicon.ico', serveStatic({ root: './' }));
-app.get('/static/*', serveStatic({ root: './' }));
 app.get('*', async (c) => {
 	const res: Response = await fetch(c.req.url);
 	const acceptHeader: string | undefined = c.req.header('Accept');
-	const isHTMLContentTypeAccepted: Boolean = typeof acceptHeader === 'string' && acceptHeader.indexOf('text/html') >= 0;
 
+	const isHTMLContentTypeAccepted: Boolean = typeof acceptHeader === 'string' && acceptHeader.indexOf('text/html') >= 0;
 	if (isHTMLContentTypeAccepted) {
 		switch (res.status) {
 			case 404: {
-				return c.redirect('/static/404.html', 302);
+				return new Response(html404, { status: 404, headers: res.headers });
 				break;
 			}
 			case 500: {
-				c.status(500);
-				return c.redirect('/static/500.html', 302);
+				return new Response(html500, { status: 500, headers: res.headers });
 				break;
 			}
 			default: {
